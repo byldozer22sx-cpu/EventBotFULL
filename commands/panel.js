e7527a14b950569bcc0dbc65728d602f88d1c888
byldozer22sx-cpu <byldozer22sx@gmail.com>
2026-06-28 23:53:07 +0300

    Add files via upload

---
+++ commands/panel.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { sendPanel } = require('../utils/eventManager');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('panel')
+    .setDescription('Создать панель записи на ивент')
+    .addIntegerOption(option =>
+      option
+        .setName('slots')
+        .setDescription('Сколько мест создать')
+        .setRequired(true)
+        .setMinValue(1)
+        .setMaxValue(25)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction) {
+    await sendPanel(interaction);
+  }
+};
