94296def175272e4f8735283c623eac9f6c2b06e
byldozer22sx-cpu <byldozer22sx@gmail.com>
2026-06-28 22:49:11 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot is running');
+}).listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
 const fs = require('fs');
 const path = require('path');
 const { Client, Collection, GatewayIntentBits, Events } = require('discord.js');
