4b3309ba59170cdf338f3011f3b1d15860f1e621
byldozer22sx-cpu <byldozer22sx@gmail.com>
2026-06-28 23:45:11 +0300

    panel.js

--- commands/panel.js
+++ commands/panel.js
@@ -5,7 +5,16 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('panel')
     .setDescription('Создать панель записи на ивент')
+    .addIntegerOption(option =>
+      option
+        .setName('slots')
+        .setDescription('Сколько мест создать')
+        .setRequired(true)
+        .setMinValue(1)
+        .setMaxValue(100)
+    )
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
   async execute(interaction) {
     await sendPanel(interaction);
   }
